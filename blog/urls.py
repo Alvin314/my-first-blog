9f8c7e051024de4e2b421484eb3c6c905191ebdb
Alvin <alvhor02@edu.umea.se>
2018-05-31 14:41:18 +0200

    Nytt namn post är nu games

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url('^posts/$', views.post_list, name='post_list'),
+    url('^games/$', views.post_list, name='post_list'),
 ]
